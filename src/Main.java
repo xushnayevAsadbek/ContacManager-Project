e3c517b4ca8e27cabba05de9ee82e6165e55fcef
Asadbek Xushnayev <xushnaevasadbek@gmail.com>
2025-02-20 20:16:34 +0500

    contact qoshish

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         boolean b = true;
+        Contact [] contactArray = new Contact[10];
+        int contactIndex = 0;
         while (b) {
             menu();
             int n = getMenuNumber();
             switch (n) {
                 case 1:
                     Contact contact = addContact();
+                    if (isValidContact(contact)) {
+                        contactArray[contactIndex] = contact;
+                        contactIndex ++ ;
                     System.out.println(contact.name + " " + contact.surname);
+
+                     } 
+
                     break;
                     case 2:
                     
@@ -24,26 +32,12 @@ public class Main {
                     b= false;
                     break;
                 default:
-                    System.out.println("Please choose current ");
+                    System.out.println("Please choose current number ");
             }
-            System.out.println("The end");
         }
-    }
+        System.out.println("The end");
 
-    public static void menu (){
-          System.out.println("** Menu **");  
-          System.out.println("1. Add contact");  
-          System.out.println("2. Add List ");  
-          System.out.println("3. Search ");  
-          System.out.println("4. Delete Contact ");  
-          System.out.println("0. Exit ");  
     }
-    public static int getMenuNumber(){
-        System.out.print("Choose menu: ");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
-    }
-
     public static Contact addContact(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter name: ");
@@ -61,4 +55,44 @@ public class Main {
         contact.phone = phone;
         return contact;
     }
+
+    public static boolean isValidContact(Contact contact){
+        if (contact.name == null || contact.name.trim().length()<3) {
+            System.out.println("Contact name is wrong");
+            return false;
+        }
+        if (contact.surname == null || contact.surname.trim().length()<3) {
+            System.out.println("Contact surname is wrong");
+            return false;
+        }
+        if (contact.phone == null || contact.phone.trim().length() !=12 || !contact.phone.startsWith("998")) {
+            System.out.println("Contact phone is wrong");
+            return false;
+        }
+        char [] phoneArr = contact.phone.toCharArray();
+        for(char c: phoneArr){
+            if(!Character.isDigit(c)){
+                System.out.println("Contact phone is wrong");
+            return false;
+            }
+        }
+            return true ;
+    }
+
+    public static void menu (){
+          System.out.println("** Menu **");  
+          System.out.println("1. Add contact");  
+          System.out.println("2. Add List ");  
+          System.out.println("3. Search ");  
+          System.out.println("4. Delete Contact ");  
+          System.out.println("0. Exit ");  
+    }
+    public static int getMenuNumber(){
+        System.out.print("Choose menu: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+   
+   
 }
