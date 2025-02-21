fbf602f04661b5d1d07fe156bfd38c39f8756476
Asadbek Xushnayev <xushnaevasadbek@gmail.com>
2025-02-21 18:58:42 +0500

    codelarni alohida methodlarga olindi

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
+    static Contact [] contactArray = new Contact[10];
+    static int contactIndex = 0;
     public static void main(String[] args) {
         boolean b = true;
-        Contact [] contactArray = new Contact[1];
-        int contactIndex = 0;
+       
         while (b) {
             menu();
             int n = getMenuNumber();
             switch (n) {
                 case 1:
                     Contact contact = addContact();
-                    if (isValidContact(contact)) {
-                        if(contactIndex == contactArray.length){
-                            Contact[] newArr = new Contact[contactArray.length * 2];
-                            for(int i =0 ; i<contactArray.length ; i++){
-                                newArr[i] = contactArray[i];
-                            }
-                            contactArray = newArr;
-                            System.out.println("New Array Created");
-                        }
-                        
-
-                        contactArray[contactIndex] = contact;
-                        contactIndex ++ ;
-                    System.out.println("Contact added. ");
-
-                     } 
+                    addToArray(contact);
 
                     break;
                     case 2:
-                    for(Contact c : contactArray){
-                        if (c != null) {
-                            System.out.println(c.name + " " + c.surname + " " + c.phone);
-                        }
-                    }
+                    printContactList();
                     break;
                     case 3:
                     
@@ -93,6 +75,32 @@ public class Main {
             return true ;
     }
 
+    public static void addToArray(Contact contact){
+        if (isValidContact(contact)) {
+            if(contactIndex == contactArray.length){
+                Contact[] newArr = new Contact[contactArray.length * 2];
+                for(int i =0 ; i<contactArray.length ; i++){
+                    newArr[i] = contactArray[i];
+                }
+                contactArray = newArr;
+                System.out.println("New Array Created");
+            }
+            
+
+            contactArray[contactIndex] = contact;
+            contactIndex ++ ;
+        System.out.println("Contact added. ");
+
+         } 
+    }
+
+    public static void printContactList(){
+        for(Contact c : contactArray){
+            if (c != null) {
+                System.out.println(c.name + " " + c.surname + " " + c.phone);
+            }
+        }
+    }
     public static void menu (){
           System.out.println("** Menu **");  
           System.out.println("1. Add contact");  
